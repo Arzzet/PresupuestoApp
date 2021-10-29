a46bd36487df5de035fff00165d6371a92c20851
Iwo <iwoskater3@hotmail.com>
2021-10-29 21:48:38 +0200

    app structure set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { GastoComponent } from './gasto/gasto.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IngresoComponent,
+    GastoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
