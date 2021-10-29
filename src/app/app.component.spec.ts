dc30d139e51d2edb71bc932a9393c77f8f7f60aa
Iwo <iwoskater3@hotmail.com>
2021-10-29 21:36:43 +0200

    Visual style applied w/placeholders

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PresupuestoApp'`, () => {
+  it(`should have as title 'presupuesto-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PresupuestoApp');
+    expect(app.title).toEqual('presupuesto-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PresupuestoApp app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('presupuesto-app app is running!');
   });
 });
